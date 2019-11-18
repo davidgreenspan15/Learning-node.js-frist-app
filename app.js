db0317f03dd85439bc1b3166489bbb03fb2cfe41
davidgreenspan15 <ramigreenspan@gmail.com>
2019-11-18 07:31:38 -0500

    requiring modules

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// import log function from logger.js
 const log = require('./logger.js')
 
 function sayHello(name){
@@ -6,3 +7,23 @@ function sayHello(name){
 sayHello('World')
 
 log('message')
+
+// learning to require modules
+const path = require('path')
+const pathObj = path.parse(__filename)
+ console.log(pathObj);
+ // learning to require os
+
+const os = require('os')
+const totalMemory = os.totalmem()
+const freeMemory = os.freemem()
+
+console.log(totalMemory);
+console.log(freeMemory);
+
+// learning to require filesystem
+const fs = require('fs')
+
+const files = fs.readdirSync('./')
+
+console.log(files);
